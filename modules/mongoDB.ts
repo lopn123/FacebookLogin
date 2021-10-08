d4b83c54dd8ee5563349d71a42859036a4cc7eec
lopn123 <67193229+lopn123@users.noreply.github.com>
2021-10-08 11:11:27 +0900

    [fix] Connect MongoDB Test (3)

--- modules/mongoDB.ts
+++ modules/mongoDB.ts
@@ -10,7 +10,7 @@ export default function mongooseInit()
 
 function connect()
 {
-    mongoose.connect('mongodb://localhost:3000/test', (err) => {
+    mongoose.connect('mongodb://localhost:27017/test', (err) => {
         if(err)
         {
             console.log(err);
